45e8095b8758fb648a9b1e5de17ee812a9916dde
huangfusuper <huangfusuper@163.com>
2021-02-20 11:03:48 +0800

    项目初始化

---
+++ src/main/java/com/huangfu/assembly/SpringbootAssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.huangfu.assembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAssemblyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAssemblyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huangfu/assembly/SpringbootAssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangfu.assembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAssemblyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
